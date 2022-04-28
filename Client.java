a1891592d9bb3c5b3b0c03588147063ab5ef6d66
ArtemDamrin <damrin.a@yandex.ru>
2022-04-28 23:54:45 +0300

    first commit

---
+++ Client.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+    public class Client {
+        public static void main(String[] args) throws IOException {
+            try (
+                    Socket socket = new Socket("localhost", 11111);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            ) {
+                out.println("Artem");
+                System.out.println(in.readLine());
+            }
+        }
+
+    }
+
